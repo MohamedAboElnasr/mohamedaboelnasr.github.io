01392e61cfb80a187910036b09e91d0940586f6c
MohamedAboElnasr <ggta8080@gmail.com>
2025-12-27 00:03:16 +0200

    fixing js code

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766786374',resources: [
+    cacheName: 'chirpy-1766786558',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
