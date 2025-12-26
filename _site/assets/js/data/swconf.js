07823af6122e70a266c70200646f0853cc11ca60
MohamedAboElnasr <ggta8080@gmail.com>
2025-12-26 23:42:57 +0200

    solving prob

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766784429',resources: [
+    cacheName: 'chirpy-1766785348',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
