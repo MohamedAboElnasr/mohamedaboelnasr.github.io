dc3a58229191e43384338c45497d34d1cb8bb731
MohamedAboElnasr <ggta8080@gmail.com>
2025-12-26 23:56:10 +0200

    updating design part2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766785348',resources: [
+    cacheName: 'chirpy-1766786124',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
